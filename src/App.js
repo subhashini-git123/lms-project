85d11a5c0e8392847c420c8e4bfd697c153fe8c0
Subhashini <subhashini.m@lucidatechnologies.com>
2025-06-13 10:32:50 +0530

    FEAT:Check email added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
 import "./App.css";
+import CheckEmail from "./components/CheckEmail";
  
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+             <Route path="/check-email" element={<CheckEmail/>} />
       </Routes>
     </Router>
   );
--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,24 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
- 
-function ForgotPassword() {
+import { Link, useNavigate } from "react-router-dom";
+
+const ForgotPassword = () => {
+  const navigate = useNavigate();
+
+  const handleSendMail = () => {
+    navigate('/check-email');
+  };
+
   return (
     <div className="login-container">
       <h2>Forgot Password</h2>
- 
+
       <label>Enter your email</label>
       <input type="email" placeholder="Enter your email" />
- 
+
       <label>Password</label>
       <input type="password" placeholder="Enter new password" />
- 
-      <button className="login-button">Send Mail</button>
- 
+
+      <button className="login-button" onClick={handleSendMail}>Send Mail</button>
+
       <div className="back-link">
         <Link to="/">← Back to Login</Link>
       </div>
     </div>
   );
-}
- 
+};
+
 export default ForgotPassword;
+
