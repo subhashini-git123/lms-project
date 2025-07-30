2b0dc334a9be1a7d3fb2c9ecedffbd3ef75a0ea4
Subhashini <subhashini.m@lucidatechnologies.com>
2025-07-30 10:44:19 +0530

    Media queries added

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -19,5 +19,49 @@ const Toggle = ({ children }) => {
     </div>
   );
 };
-
 export default Toggle;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import Sidebar from "./Sidebar";
+// import "./Toggle.css";
+
+// const Toggle = ({ children }) => {
+//   const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+//   const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
+
+//   return (
+//     <div className="toggle-layout">
+//       <button className="hamburger" onClick={toggleSidebar}>
+//         ☰
+//       </button>
+
+//       {isSidebarOpen && <Sidebar onClose={toggleSidebar} showClose={true} className="open" />}
+
+//       <div className="toggle-content">{children}</div>
+//     </div>
+//   );
+// };
+
+// export default Toggle;
